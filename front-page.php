0eae4c9c6fae35f3f9d12a657b6a3ad0e823e868
JOHN ESPINDOLA <JESPINDOLA@ddb.local>
2020-06-11 16:14:11 -0500

    styles checkout & front-page

--- front-page.php
+++ front-page.php
@@ -1,6 +1,6 @@
 <?php
 
-get_header();?>
+get_header('test');?>
 
 <!-- <div class="container">
     <h1>
@@ -11,80 +11,27 @@ get_header();?>
 
 <section class="container-fluid main">
 
-<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-  <div class="carousel-inner">
-    <!-- 1 Item Slider -->
-    <div class="carousel-item active">
-        <div class="header-image slider-image-1"></div>  
-            <div class="carousel-caption d-none d-md-block">
-            <div class="wrap-animation">
-                <div class="row">
-                    <div class="col-6 header-message">
-                        <h1>NUESTRO TRABAJO SALVA VIDAS</h1>
-                        <div class="title-separator mt-4"></div>
-                        <div class="mt-4">
-                            <h3>
-                                <span class="header-sub-text">
-                                    Comprometidos con la salud de todos los colombianos
-                                </span>
-                            </h3>    
-                        </div>
-                    </div>
-                </div>    
-            </div>
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+        <img src="http://ddb.test/wp-content/uploads/2020/06/DDB_front-page2.jpg" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+        <img src="http://ddb.test/wp-content/uploads/2020/06/DDB_front-page2.jpg" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+        <img src="http://ddb.test/wp-content/uploads/2020/06/DDB_front-page2.jpg" class="d-block w-100" alt="...">
         </div>
     </div>
-    <!-- 2 Item Slider -->
-    <div class="carousel-item ">
-        <div class="header-image slider-image-1"></div>  
-            <div class="carousel-caption d-none d-md-block">
-            <div class="wrap-animation">
-                <div class="row">
-                    <div class="col-6 header-message">
-                        <h1>NUESTRO TRABAJO SALVA VIDAS</h1>
-                        <div class="title-separator mt-4"></div>
-                        <div class="mt-4">
-                            <h3>
-                                <span class="header-sub-text">
-                                    Comprometidos con la salud de todos los colombianos
-                                </span>
-                            </h3>    
-                        </div>
-                    </div>
-                </div>    
-            </div>
-        </div>
+    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
     </div>
-    <!-- 3 Item Slider -->
-    <div class="carousel-item ">
-        <div class="header-image slider-image-1"></div>  
-            <div class="carousel-caption d-none d-md-block">
-            <div class="wrap-animation">
-                <div class="row">
-                    <div class="col-6 header-message">
-                        <h1>NUESTRO TRABAJO SALVA VIDAS</h1>
-                        <div class="title-separator mt-4"></div>
-                        <div class="mt-4">
-                            <h3>
-                                <span class="header-sub-text">
-                                    Comprometidos con la salud de todos los colombianos
-                                </span>
-                            </h3>    
-                        </div>
-                    </div>
-                </div>    
-            </div>
-        </div>
-    </div>
-  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
 
 </section>
 
---
+++ header-test.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ */
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
+	<title>Document</title>
+	<?php wp_head(); ?>
+
+</head>
+<body>
+
+<div id="preloader">
+		<div id="status">
+	  </div> 
+</div>
+
+<style>
+    
+</style>
+
+
+<header class="header-test">
+		<div class="top-bar-test">
+			<div class="container d-flex justify-content-between align-items-center">
+				<div>
+					<span class="pr-3">
+						<i class="far fa-clock"></i>
+						Mar - Sab 7am - 4pm
+					</span>
+					<span class="border-left border-dark pl-3 pr-3">
+						<i class="fas fa-phone"></i>	
+						<a href="tel:(1)7432597">(1) 743 25 97</a>
+					</span>
+					<span class="border-left border-dark pl-3 pr-3">
+						<i class="far fa-envelope"></i>
+						<a href="mailto:info@ddb.com.co">info@ddb.com.co</a>
+					</span>
+					<span class="border-left border-dark pl-3">
+						<i class="fas fa-map-marker-alt"></i>
+						<a href="https://www.google.com/maps/place/Parque+Empresarial+Puerta+del+Sol/@4.6861636,-74.1238557,17z/data=!3m1!4b1!4m5!3m4!1s0x8e3f9b451d6a85f3:0x7213b2e152566ddf!8m2!3d4.6861583!4d-74.121667" target="_blank">
+							Tv. 93 # 51-98 Bodega 18 Bogotá
+						</a>
+					</span>
+				</div>
+				<div>
+					<span class="header-social">
+						<?php echo do_shortcode('[wcas-search-form]'); ?>
+					</span>
+				</div>
+
+			</div>
+	</div>
+
+
+	<nav class="navbar navbar-expand-lg navbar-dark" role="navigation">
+	<div class="container">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<a class="navbar-brand" href="<?php echo home_url(); ?>">
+				<img src="<?php echo(get_site_url() . '/wp-content/uploads/2020/06/deposito-de-drogas-Boyaca-logo.png');?>" alt="" class="nav-logo">
+			</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="Toggle navigation"> 
+			<span class="navbar-toggler-icon"></span>
+
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+			
+			</button>
+			<div class="collapse navbar-collapse flex-grow-0" id="bs-example-navbar-collapse-1">
+				<?php
+				wp_nav_menu( array(
+					'theme_location'    => 'primary',
+					'depth'             => 3,
+					'container'         => 'nav',
+					'container_class'   => 'collapse navbar-collapse',
+					'container_id'      => 'bs-example-navbar-collapse-1',
+					'menu_class'        => 'nav navbar-nav text-right',
+					'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+					'walker'            => new WP_Bootstrap_Navwalker(),
+				));
+				?>
+		</div>
+		</div>
+	</nav>
+
+		
+</header>
